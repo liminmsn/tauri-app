dfb7fe1ae62d10135fa73e0f64a526f5df2214a0
杨再涛 <1772081518@qq.com>
2025-07-31 12:06:44 +0800

    添加http模块

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -7,31 +7,13 @@
 
 <script setup lang="ts">
 import { ref } from "vue";
-import { invoke } from "@tauri-apps/api/core";
 import { Button } from "ant-design-vue";
-import { WebviewWindow } from "@tauri-apps/api/webviewWindow";
-const greetMsg = ref("");
-const name = ref("");
-async function greet_() {
-  greetMsg.value = await invoke("greet", { name: name.value });
-}
+import { fetch } from '@tauri-apps/plugin-http';
 function open() {
-  const child = new WebviewWindow('second-window', {
-    url: 'https://poki.com/zh',
-    width: 800,
-    height: 600,
-    // title: '第二个窗口',
-    resizable: true,
-    fullscreen: true,
-  });
-
-  child.once('tauri://created', function () {
-    console.log('Second window created');
-  });
-
-  child.once('tauri://error', function (e) {
-    console.error('Failed to create window:', e);
-  });
+  fetch("https://www2.jiumodiary.com/init_hubs.php", {
+    method: 'POST',
+    body:null
+  })
 }
 </script>
 
