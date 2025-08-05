5200fa12a2955c2487a376980df31130213cab7e
杨再涛 <1772081518@qq.com>
2025-08-05 17:22:57 +0800

    初始化发布版本

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -13,7 +13,6 @@ export function rdmColor() {
 }
 
 export function rdmColorTwo() {
-    // const colors = ['#F1F8E9','#FDF5E6','#F3E5F5','#F0F4C3','#E0F2F1'];
-    const colors = ['#C5E1A5','#E0C4A0','#D1C4E9','#AED581','#80CBC4'];
+    const colors = ['#FE7743', '#8AA624', '#799EFF'];
     return colors[Math.floor(colors.length * Math.random())];
 }
