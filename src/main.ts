23d7ff3ed84d3c9b685b0656291a983c49f60da3
杨再涛 <1772081518@qq.com>
2025-08-12 15:42:09 +0800

    添加重写样式封装

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import '@/design/main.css';
 import 'ant-design-vue/dist/reset.css';
-import 'virtual:uno.css'
+import 'virtual:uno.css';
 
 createApp(App)
 .use(router)
