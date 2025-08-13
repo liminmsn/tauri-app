5b03e2e360b006b33cd4b288150568490f3f3b78
杨再涛 <1772081518@qq.com>
2025-08-14 01:10:25 +0800

    滚动回到顶部

--- src/views/Home/control/main.vue
+++ src/views/Home/control/main.vue
@@ -1,5 +1,5 @@
 <template>
-    <Card class="main flex-1 ml-1 shadow-md overflow-auto" :ref="main">
+    <Card class="main flex-1 ml-1 shadow-md overflow-auto" id="main">
         <a-watermark class="h-full" :content="water_mark">
             <div class="text-center" v-if="isLod">
                 <a-spin class="mt-50" />
@@ -12,15 +12,14 @@
                 <a-empty :image="simpleImage" :description="main_tips" />
             </div>
         </a-watermark>
-        <a-float-button-group shape="square" :style="{ right: '10px', bottom: '15px' }">
-            <a-back-top :visibility-height="0" />
-        </a-float-button-group>
+        <BackTop v-if="topBtn" shape="square" :visibility-height="0" @click="scrollTop"
+            :style="{ right: '10px', bottom: '15px' }" />
     </Card>
 </template>
 
 <script setup lang="ts">
 import { onMounted, ref } from 'vue';
-import { Card, Empty } from 'ant-design-vue';
+import { BackTop, Card, Empty } from 'ant-design-vue';
 import { GlobalEvent } from '@/event/GlobalEvent';
 import { HomeNet } from '@/net/api/HomeNet';
 import { current, SearchHistory } from '../script';
@@ -28,13 +27,30 @@ import Html_text from './components/html_text.vue';
 const water_mark = import.meta.env['VITE_MAIN_WATER_MARK'];
 const main_tips = import.meta.env['VITE_MAIN_TIPS'];
 const simpleImage = Empty.PRESENTED_IMAGE_SIMPLE;
+const topBtn = ref(false);
 const isLod = ref(false);
 const domData = ref('');
-const main = ref();
 
+let dom: HTMLElement | null;
+onMounted(() => {
+    dom = document.getElementById('main');
+    if (dom) {
+        dom.onscroll = (ev: Event) => {
+            const ent = (ev.target as HTMLDivElement);
+            const top = ent.scrollTop;
+            const div = ent.clientHeight;
+            // console.log(top, div);
+            topBtn.value = top > div;
+        }
+    }
+})
+// 滚动到顶部
+function scrollTop() {
+    if (dom) {
+        dom.scroll({ top: 0, left: 0, behavior: 'smooth' })
+    }
+}
 GlobalEvent.on('y_input', async ({ detail }: CustomEvent) => {
-    console.log(main);
-    
     isLod.value = true;
     const text = await new HomeNet(detail['searchValue']).getData()
     domData.value = text;
