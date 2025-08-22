7e6843cda214dca50c3f4845d65f0e6112ce35d3
杨再涛 <1772081518@qq.com>
2025-08-22 16:59:09 +0800

    清理结构

--- src/view/index.vue
+++ src/view/index.vue
@@ -1,12 +1,6 @@
 <template>
   <a-dropdown :trigger="['contextmenu']">
     <main class="p1 w-full flex" style="height: calc(100% - 28px);">
-      <Card class="p-2 h-full w-40 mr-1">
-        <Button type="primary" @click="obj.emit('a', { a: 'hello world' })">start</Button>
-      </Card>
-      <Card class="p-2 h-full w-full">
-        {{ label }}
-      </Card>
     </main>
     <template #overlay>
       <a-menu>
@@ -19,13 +13,5 @@
 </template>
 
 <script setup lang="ts">
-import { useGlobalEvent } from "@/event/GlobalEvent";
-import { Button, Card } from "ant-design-vue";
-import { ref } from "vue";
-const label = ref('');
-const obj = useGlobalEvent();
-obj.on('a', data => {
-  console.log(data);
-});
 </script>
 <style scoped></style>
